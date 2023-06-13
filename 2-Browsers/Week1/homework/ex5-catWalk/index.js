ba79fac808536329b743a953adce353c9a3122c7
Robert Okuni <o.robert1994@htomail.com>
2023-06-14 01:45:42 +0200

    robert-browsers

--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -21,8 +21,39 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
 
    https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif
 -----------------------------------------------------------------------------*/
-function catWalk() {
-  // TODO complete this function
-}
 
-// TODO execute `catWalk` when the browser has completed loading the page
+// variable to store img element
+const imgElement = document.querySelector('img');
+imgElement.style.left = '0px';
+// maximum screen position
+const fullScreen = window.innerWidth;
+const middlePosition = fullScreen / 2;// divide it by half
+
+let Dancing = false;
+
+
+function catWalk() {
+  // function  to move the cat 10 pixels to the right
+  const currentPosition = parseInt(imgElement.style.left);//parseInt to pass in the string value as assigned decimal value..google source
+  const newPosition = currentPosition + 10;
+  // Move the cat to the right by updating the left position
+  imgElement.style.left = `${newPosition}px`;
+  
+  // this returns the cat to the begining
+  if (newPosition === fullScreen) {
+    imgElement.style.left = '0px';
+  }
+  if (newPosition === middlePosition && !Dancing) {
+    // If the cat reaches the middle of the screen and is not already dancing
+    imgElement.src = 'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+    Dancing = true;
+    // After 5 seconds, replace the image with the original one and allow the cat to continue walking
+    setTimeout(() => {
+    imgElement.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif'; 
+    Dancing = false;
+    }, 5000);
+  } 
+    
+}
+// the interval to call the catWalk fuction every 50 mil
+setInterval(catWalk, 50);
