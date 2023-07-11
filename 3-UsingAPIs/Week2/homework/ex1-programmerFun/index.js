e507b05dc3910faa8a00b49b59f31602465bdedd
Robert Okuni <o.robert1994@htomail.com>
2023-07-11 16:28:17 +0200

    home-work-api-robert

---
+++ 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
@@ -0,0 +1,61 @@
+"use strict";
+/*------------------------------------------------------------------------------
+Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-UsingAPIs/Week2/README.md#exercise-1-programmer-fun
+
+1. Complete the function `requestData()` using `fetch()` to make a request to 
+   the url passed to it as an argument. The function should return a promise. 
+   Make sure that the promise is rejected in case of HTTP or network errors.
+2. Notice that the function `main()` calls `requestData()`, passing it the url 
+   `https://xkcd.now.sh/?comic=latest`. Try and run the code in the browser and 
+   open the browser's console to inspect the data returned from the request.
+3. Next, complete the function `renderImage()` to render an image as an `<img>` 
+   element appended to the document's body, using the data returned from the API.
+4. Complete the function `renderError()` to render any errors as an `<h1>` 
+   element appended to the document's body.
+5. Refactor the `main()` function to use `async/await`.
+6. Test error handling, for instance, by temporarily changing the `.sh` in the 
+   url with `.shx`. There is no server at the modified url, therefore this 
+   should result in a network (DNS) error.
+------------------------------------------------------------------------------*/
+function requestData(url) {
+  return Promise((resolve, reject) => {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw error(`error  encountered!`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+function renderImage(data) {
+  const image = document.createElement("img");
+  image.src = data.img;
+  document.body.appendChild(image);
+  console.log(data);
+}
+
+function renderError(error) {
+  const errorHeading = document.createElement("h1");
+  errorHeading.textContent = error.message;
+  document.body.appendChild(errorHeading);
+
+  console.log(error);
+}
+
+async function main() {
+  try {
+    const data = await requestData("https://xkcd.now.sh/?comic=latest");
+    renderImage(data);
+  } catch (error) {
+    renderError(error);
+  }
+}
+window.addEventListener("load", main);
